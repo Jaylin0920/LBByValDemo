b38504eb5da80e640b01a9b77da54cc1e92a3abd
Jaylin <Jaylin@example.com>
2016-03-15 14:05:04 +0800

    Initial Commit

---
+++ LBByValue/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LBByValue
+//
+//  Created by LB on 16/3/15.
+//  Copyright © 2016年 LB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LBByValue/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LBByValue
+//
+//  Created by LB on 16/3/15.
+//  Copyright © 2016年 LB. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
